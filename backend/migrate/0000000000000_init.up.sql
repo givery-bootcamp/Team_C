13c92307bf4f927edd20d1f994d4101d888fd4fe
tomoya-kawaguchi <tomoya.kawaguchi@dena.jp>
2024-05-27 18:05:10 +0900

    chore: 初期のSQLスキーマ定義をgo-migrateに認識させる

--- backend/migrate/0000000000000_init.up.sql
+++ backend/migrate/0000000000000_init.up.sql
@@ -1,10 +1,11 @@
-CREATE DATABASE IF NOT EXISTS training;
 USE training;
 
 CREATE TABLE IF NOT EXISTS hello_worlds(
   lang    VARCHAR(2) NOT NULL PRIMARY KEY,
   message VARCHAR(40) NOT NULL
 );
+INSERT INTO hello_worlds (lang, message) VALUES ('en', 'Hello World');
+INSERT INTO hello_worlds (lang, message) VALUES ('ja', 'こんにちは 世界');
 
 CREATE TABLE IF NOT EXISTS users(
   id         INT          AUTO_INCREMENT PRIMARY KEY,
@@ -14,6 +15,8 @@ CREATE TABLE IF NOT EXISTS users(
   updated_at DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   deleted_at DATETIME     NULL
 );
+INSERT INTO users (name, password) VALUES ('taro', 'password');
+INSERT INTO users (name, password) VALUES ('hanako', 'PASSWORD');
 
 CREATE TABLE IF NOT EXISTS posts(
   id         INT          AUTO_INCREMENT PRIMARY KEY,
@@ -25,3 +28,5 @@ CREATE TABLE IF NOT EXISTS posts(
   deleted_at DATETIME     NULL,
   FOREIGN KEY (user_id) REFERENCES users (id)
 );
+INSERT INTO posts (user_id, title, body) VALUES (1, 'test1', '質問1\n改行');
+INSERT INTO posts (user_id, title, body) VALUES (1, 'test2', '質問2\n改行');
