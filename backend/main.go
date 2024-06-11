290b808e9a91a29bd595395bd8ac7e92b8872b12
shake551 <hiroki.yamako@gmail.com>
2024-06-11 17:53:32 +0900

    delete unused path

--- backend/main.go
+++ backend/main.go
@@ -6,16 +6,13 @@ import (
 	"myapp/internal/infrastructure/persistence/datastore/driver"
 	"myapp/internal/interface/api/middleware"
 	"myapp/internal/interface/api/router"
-
-	swaggerfiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 //	@title			掲示板アプリ
 //	@version		バージョン(1.0)
 //	@description	3班の掲示板アプリのAPI仕様書
 
-//	@host	localhost:9000
+// @host	localhost:9000
 func main() {
 	var migrator middleware.DBMigrator = driver.MustNewMySQLMigrator("file://migrate")
 	if err := migrator.Migrate(); err != nil {
@@ -23,6 +20,5 @@ func main() {
 	}
 
 	router := router.CreateRouter()
-	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	router.Run(fmt.Sprintf("%s:%d", config.HostName, config.Port))
 }
