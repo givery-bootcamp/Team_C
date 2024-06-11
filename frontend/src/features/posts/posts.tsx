95db8383e44cbb934f410fa8adb2dc656cd0c60d
yoshiki-morii <yoshiki.morii@dena.com>
2024-06-11 16:52:59 +0900

    lint

--- frontend/src/features/posts/posts.tsx
+++ frontend/src/features/posts/posts.tsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../shared/hooks';
 import { APIService } from '../../shared/services';
 import { Post } from '../../shared/models/post';
-import { ChakraProvider ,Box, Grid} from '@chakra-ui/react';
+import { Box, Grid } from '@chakra-ui/react';
 export function Posts() {
   const { posts } = useAppSelector((state) => state.post);
   const dispatch = useAppDispatch();
@@ -12,16 +12,16 @@ export function Posts() {
     dispatch(APIService.getPosts());
   }, [dispatch]);
 
-    return (
-            <div className="posts-container">
-                <Grid templateColumns="repeat(3, 1fr)" gap={6}>
-                    {posts?.map((post: Post) => (
-                        <Box key={post.id} borderWidth="1px" borderRadius="lg" p={4}>
-                            <h2 className="post-title">{post.title}</h2>
-                            <p className="post-body">{post.body}</p>
-                        </Box>
-                    ))}
-                </Grid>
-            </div>
-    );
+  return (
+    <div className="posts-container">
+      <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+        {posts?.map((post: Post) => (
+          <Box key={post.id} borderWidth="1px" borderRadius="lg" p={4}>
+            <h2 className="post-title">{post.title}</h2>
+            <p className="post-body">{post.body}</p>
+          </Box>
+        ))}
+      </Grid>
+    </div>
+  );
 }
