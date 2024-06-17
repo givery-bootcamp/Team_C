28d52c923e59e3cbf49ca8fa2a7360ba0d3184be
shake551 <hiroki.yamako@gmail.com>
2024-06-17 12:21:20 +0900

    add allow header

--- backend/internal/interface/api/middleware/cors.go
+++ backend/internal/interface/api/middleware/cors.go
@@ -11,5 +11,12 @@ func Cors() gin.HandlerFunc {
 	config := cors.DefaultConfig()
 	config.AllowOrigins = myConfig.CorsAllowOrigin
 	config.AllowCredentials = true
+	config.AllowHeaders = []string{
+		"Access-Control-Allow-Credentials",
+		"Access-Control-Allow-Headers",
+		"Content-Type",
+		"Content-Length",
+		"Authorization",
+	}
 	return cors.New(config)
 }
