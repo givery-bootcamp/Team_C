3f4ea4c2ed19a48f2fa7e205d7ed241001c14937
yoshiki-morii <yoshiki.morii@dena.com>
2024-06-17 14:54:26 +0900

    /loginはやした

--- frontend/src/app/AppRoute.tsx
+++ frontend/src/app/AppRoute.tsx
@@ -2,11 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import { HelloWorld } from 'features/helloworld';
 import { Posts } from 'features/posts';
+import { Login } from 'features/login';
+
 export const AppRoute = () => {
   return (
     <Routes>
       <Route path="/" element={<HelloWorld />} />
       <Route path="/posts/" element={<Posts />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
