11cde0187b05a275a0a42fef34cbdfde135e575c
yoshiki-morii <yoshiki.morii@dena.com>
2024-06-17 11:19:24 +0900

    swaggerからモデルを生成

---
+++ frontend/src/api/models/model_UserSigninParam.ts
@@ -0,0 +1,9 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type model_UserSigninParam = {
+    name?: string;
+    password?: string;
+};
+
