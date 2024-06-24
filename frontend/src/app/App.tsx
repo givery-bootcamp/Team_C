17836f41620b9176dd3dea57da76e96e26c96663
yoshiki-morii <yoshiki.morii@dena.com>
2024-06-24 15:14:01 +0900

    /loginではloginボタンを非表示

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -2,15 +2,13 @@ import { AppRoute } from './AppRoute';
 
 import './App.scss';
 import { ChakraProvider } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
+import { Header } from 'components/header';
 
 function App() {
   return (
     <ChakraProvider>
       <div className="app-root">
-        <Link to="/">
-          <header className="app-header">サンプルアプリケーション</header>
-        </Link>
+        <Header />
         <main className="app-body container">
           <AppRoute />
         </main>
