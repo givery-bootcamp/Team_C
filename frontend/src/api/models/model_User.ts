57271e64af171c52560202f5ecca1f0ff8f5e9e4
yoshiki-morii <yoshiki.morii@dena.com>
2024-06-25 15:28:53 +0900

    add openapi-generator

--- frontend/src/api/models/model_User.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type model_User = {
-    id?: number;
-    name?: string;
-};
-
