d698eba2f6b58f5dc477cdad20d386877f08c216
kinakomoti-quad <52781608+kinakomoti-quad@users.noreply.github.com>
2024-06-25 15:43:17 +0900

    create postdetail page

--- frontend/src/features/postdetail/postdetail.tsx
+++ frontend/src/features/postdetail/postdetail.tsx
@@ -2,9 +2,13 @@ import { useEffect } from 'react';
 
 import { useAppDispatch, useAppSelector } from 'shared/hooks';
 import { APIService } from 'shared/services';
-import { model_Post } from 'api';
-import { Box } from '@chakra-ui/react';
+import { Box, Divider, HStack, Heading, Text, Avatar, Button, ButtonGroup } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom'
+
+function format_date(date_string:any) {
+  const date = new Date(date_string)
+  return date.toLocaleString('ja-JP')
+}
 
 export function PostDetail() {
   const {postId} = useParams()
@@ -19,8 +23,22 @@ export function PostDetail() {
 
   return(
     <Box>
-        <h1>{postdetail?.id}</h1>
-        <h1>{postdetail?.created_at}</h1>
+        <Heading as={'h1'} >{postdetail?.title}</Heading>
+        <HStack >
+          <Avatar size="sm" name={postdetail?.user?.name} />
+          <Text as={'span'}>{postdetail?.user?.name}</Text>
+        </HStack>
+        
+        <HStack spacing={10}>
+          <Text as={'span'}>作成日時 {format_date(postdetail?.created_at)}</Text>
+          <Text as={'span'}>投稿日時 {format_date(postdetail?.updated_at)}</Text>
+        </HStack>
+        <Divider />
+        <Text>{postdetail?.body}</Text>
+        <ButtonGroup colorScheme='blue'>
+          <Button>編集</Button>
+          <Button>削除</Button>
+        </ButtonGroup>
         
     </Box>
   )
