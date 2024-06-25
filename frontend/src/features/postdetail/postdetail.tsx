d45b588a26dff155892cd892eb31e138fe7e709a
kinakomoti-quad <52781608+kinakomoti-quad@users.noreply.github.com>
2024-06-25 15:52:13 +0900

    move formatDate func to services

--- frontend/src/features/postdetail/postdetail.tsx
+++ frontend/src/features/postdetail/postdetail.tsx
@@ -1,14 +1,9 @@
 import { useEffect } from 'react';
 
 import { useAppDispatch, useAppSelector } from 'shared/hooks';
-import { APIService } from 'shared/services';
+import { APIService,DateService } from 'shared/services';
 import { Box, Divider, HStack, Heading, Text, Avatar, Button, ButtonGroup } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom'
-
-function format_date(date_string:any) {
-  const date = new Date(date_string)
-  return date.toLocaleString('ja-JP')
-}
 
 export function PostDetail() {
   const {postId} = useParams()
@@ -19,8 +14,6 @@ export function PostDetail() {
     dispatch(APIService.getPostDetail(Number(postId)));
   }, [dispatch]);
 
-  // console.log(postId)
-
   return(
     <Box>
         <Heading as={'h1'} >{postdetail?.title}</Heading>
@@ -28,18 +21,16 @@ export function PostDetail() {
           <Avatar size="sm" name={postdetail?.user?.name} />
           <Text as={'span'}>{postdetail?.user?.name}</Text>
         </HStack>
-        
         <HStack spacing={10}>
-          <Text as={'span'}>作成日時 {format_date(postdetail?.created_at)}</Text>
-          <Text as={'span'}>投稿日時 {format_date(postdetail?.updated_at)}</Text>
+          <Text as={'span'}>作成日時 {DateService.formatDate(postdetail?.created_at)}</Text>
+          <Text as={'span'}>投稿日時 {DateService.formatDate(postdetail?.updated_at)}</Text>
         </HStack>
         <Divider />
         <Text>{postdetail?.body}</Text>
         <ButtonGroup colorScheme='blue'>
           <Button>編集</Button>
           <Button>削除</Button>
-        </ButtonGroup>
-        
+        </ButtonGroup>        
     </Box>
   )
 }
