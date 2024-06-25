87cf42d254eddbfd56fbbe235dbec549417b6b3b
yoshiki-morii <yoshiki.morii@dena.com>
2024-06-25 15:51:03 +0900

    fix

--- frontend/src/shared/services/API.ts
+++ frontend/src/shared/services/API.ts
@@ -1,29 +1,40 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { ApiClient } from 'api/ApiClient';
 import { Hello } from 'shared/models';
-import { model_Post } from 'api/models/model_Post';
-import { model_UserSigninParam } from 'api/models/model_UserSigninParam';
+import axios from 'axios';
+import {
+  AuthApi,
+  Configuration,
+  ModelPost,
+  ModelUserSigninParam,
+  PostApi,
+} from 'api';
 const API_ENDPOINT_PATH = import.meta.env.VITE_API_ENDPOINT_PATH ?? '';
+
+const configuration = new Configuration({
+  basePath: API_ENDPOINT_PATH,
+});
+const axiosInstance = axios.create({
+  baseURL: API_ENDPOINT_PATH,
+  withCredentials: true,
+});
 
 export const getHello = createAsyncThunk<Hello>('getHello', async () => {
   const response = await fetch(`${API_ENDPOINT_PATH}/hello`);
   return await response.json();
 });
 
-export const getPosts = createAsyncThunk<model_Post[]>('getPosts', async () => {
-  const api = new ApiClient({ BASE: API_ENDPOINT_PATH });
-  const getPostsResponse = await api.post.getApiPosts({ limit: 20, offset: 0 });
-  return await getPostsResponse;
+export const getPosts = createAsyncThunk<ModelPost[]>('getPosts', async () => {
+  const api = new PostApi(configuration, API_ENDPOINT_PATH, axiosInstance);
+  const getPostsResponse = await api.apiPostsGet(20, 0);
+  return await getPostsResponse.data;
 });
 
 export const postSignin = createAsyncThunk<
-  model_UserSigninParam,
-  model_UserSigninParam
->('postSignin', async (userSigninParam: model_UserSigninParam) => {
-  const api = new ApiClient({ BASE: API_ENDPOINT_PATH });
-  const postSignupResponse = await api.auth.postApiSignin({
-    body: userSigninParam,
-  });
+  ModelUserSigninParam,
+  ModelUserSigninParam
+>('postSignin', async (userSigninParam: ModelUserSigninParam) => {
+  const api = new AuthApi(configuration, API_ENDPOINT_PATH, axiosInstance);
+  const postSignupResponse = await api.apiSigninPost(userSigninParam);
 
-  return await postSignupResponse;
+  return await postSignupResponse.data;
 });
