ab696ad48c7a9959c71d66ccb526ee5fc214964c
shake551 <hiroki.yamako@gmail.com>
2024-07-01 14:48:10 +0900

    add mysql test

--- backend/internal/infrastructure/persistence/datastore/driver/mysql.go
+++ backend/internal/infrastructure/persistence/datastore/driver/mysql.go
@@ -4,14 +4,13 @@ package driver
 import (
 	"context"
 	"fmt"
-	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 type DB interface {
-	Transaction(ctx context.Context, f func(ctx context.Context) error) error
+	// Transaction(ctx context.Context, f func(ctx context.Context) error) error
 	GetDB(ctx context.Context) *gorm.DB
 }
 
@@ -29,18 +28,18 @@ type txContextKey struct{}
 
 var txKey = txContextKey{}
 
-func (d *db) Transaction(ctx context.Context, f func(ctx context.Context) error) error {
-	return d.conn.Transaction(func(tx *gorm.DB) error {
-		ctx := context.WithValue(ctx, txKey, tx)
-		return f(ctx)
-	})
-}
+// func (d *db) Transaction(ctx context.Context, f func(ctx context.Context) error) error {
+// 	return d.conn.Transaction(func(tx *gorm.DB) error {
+// 		ctx := context.WithValue(ctx, txKey, tx)
+// 		return f(ctx)
+// 	})
+// }
 
 func (d *db) GetDB(ctx context.Context) *gorm.DB {
-	tx, ok := ctx.Value(txKey).(*gorm.DB)
-	if ok && tx != nil {
-		return tx.WithContext(ctx)
-	}
+	// tx, ok := ctx.Value(txKey).(*gorm.DB)
+	// if ok && tx != nil {
+	// 	return tx.WithContext(ctx)
+	// }
 
 	return d.conn.WithContext(ctx)
 }
@@ -49,7 +48,7 @@ func initDB() *gorm.DB {
 	db, err := gorm.Open(mysql.Open(createDSNForGorm()), &gorm.Config{})
 	if err != nil {
 		fmt.Println(err)
-		os.Exit(1)
+		panic("error open mysql")
 	}
 
 	return db
