8afb95bc43a4e0d9e6c58c13d3c443c614a18b94
tomoya-kawaguchi <tomoya.kawaguchi@dena.jp>
2024-07-01 14:50:56 +0900

    カバレッジ上昇

--- backend/internal/infrastructure/persistence/datastore/driver/dsn_test.go
+++ backend/internal/infrastructure/persistence/datastore/driver/dsn_test.go
@@ -11,3 +11,9 @@ func TestCreateDSNForGoMigrate(t *testing.T) {
 
 	assert.Equal(t, "mysql://root:password@tcp(db:3306)/training?charset=utf8mb4&parseTime=True&loc=Local", dsn)
 }
+
+func TestCreateDSNForGorm(t *testing.T) {
+	dsn := createDSNForGorm()
+
+	assert.Equal(t, "root:password@tcp(db:3306)/training?charset=utf8mb4&parseTime=True&loc=Local", dsn)
+}
