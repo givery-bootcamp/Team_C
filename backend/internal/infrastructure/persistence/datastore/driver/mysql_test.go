d4901d777645e26611e5934ea18d45d5493dfd88
shake551 <hiroki.yamako@gmail.com>
2024-07-01 14:50:15 +0900

    fix mysql test

--- backend/internal/infrastructure/persistence/datastore/driver/mysql_test.go
+++ backend/internal/infrastructure/persistence/datastore/driver/mysql_test.go
@@ -5,14 +5,10 @@ import (
 )
 
 func TestInitDB_Error(t *testing.T) {
-	t.Run(tt.name, func(t *testing.T) {
-		if tt.wantErr {
-			defer func() {
-				if r := recover(); r == nil {
-					t.Errorf("The code did not panic as expected")
-				}
-			}()
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic as expected")
 		}
-		_ = initDB()
-	})
+	}()
+	_ = initDB()
 }
